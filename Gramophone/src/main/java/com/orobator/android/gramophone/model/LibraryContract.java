63034b1db959c16860a39696b464815d5d6c72c7
Andrew Orobator <Orobator.Andrew@gmail.com>
2013-11-23 13:25:32 -0500

    Changed Navigation Hamburger icon from gray to white. Added beginning of new SQL Library code/resources needed for the code.

---
+++ Gramophone/src/main/java/com/orobator/android/gramophone/model/LibraryContract.java
@@ -0,0 +1,24 @@
+package com.orobator.android.gramophone.model;
+
+/**
+ * The LibraryContract specifies the schemas of the SQL tables used for
+ * accessing collections of music.
+ */
+public class LibraryContract {
+    /** Resources:
+     * SQL Tables: http://developer.android.com/training/basics/data-storage/databases.html
+     * MetaDataRetriever: http://developer.android.com/reference/android/media/MediaMetadataRetriever.html
+     * MediaPlayer: http://developer.android.com/reference/android/media/MediaPlayer.html
+     * MediaPlayer.TrackInfo: http://developer.android.com/reference/android/media/MediaPlayer.TrackInfo.html
+     * MediaStore: http://developer.android.com/reference/android/provider/MediaStore.html*/
+
+
+
+    /* I'm probably going to need a couple of different table types.
+    * Table type 1: Songs
+    * Table type 2: Artists
+    * Table type 3: Genres
+    * Table type 4: Albums
+    * Table type 5: Playlists
+    * */
+}
