bc9228b5a43ccbf484ef12033675a71fedec8489
Andrew Orobator <Orobator.Andrew@gmail.com>
2014-01-09 18:24:07 -0500

    Fixed crashing when CursorLoader resets for fragments that display song collections.

--- Gramophone/src/main/java/com/orobator/android/gramophone/view/fragments/SongsFragment.java
+++ Gramophone/src/main/java/com/orobator/android/gramophone/view/fragments/SongsFragment.java
@@ -4,11 +4,13 @@ import android.app.Fragment;
 import android.app.LoaderManager;
 import android.content.Loader;
 import android.database.Cursor;
+import android.database.DataSetObserver;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.ListAdapter;
 
 import com.fortysevendeg.android.swipelistview.SwipeListView;
 import com.orobator.android.gramophone.R;
@@ -43,7 +45,67 @@ public class SongsFragment extends Fragment implements LoaderManager.LoaderCallb
     @Override
     public void onLoaderReset(Loader<Cursor> loader) {
         // Stop using the cursor (via the adapter)
-        mSwipeListView.setAdapter(null);
+        mSwipeListView.setAdapter(new ListAdapter() {
+            @Override
+            public boolean areAllItemsEnabled() {
+                return false;
+            }
+
+            @Override
+            public boolean isEnabled(int position) {
+                return false;
+            }
+
+            @Override
+            public void registerDataSetObserver(DataSetObserver observer) {
+                // Do Nothing
+            }
+
+            @Override
+            public void unregisterDataSetObserver(DataSetObserver observer) {
+                // Do Nothing
+            }
+
+            @Override
+            public int getCount() {
+                return 0;
+            }
+
+            @Override
+            public Object getItem(int position) {
+                return null;
+            }
+
+            @Override
+            public long getItemId(int position) {
+                return 0;
+            }
+
+            @Override
+            public boolean hasStableIds() {
+                return false;
+            }
+
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                return null;
+            }
+
+            @Override
+            public int getItemViewType(int position) {
+                return 1;
+            }
+
+            @Override
+            public int getViewTypeCount() {
+                return 1;
+            }
+
+            @Override
+            public boolean isEmpty() {
+                return false;
+            }
+        });
     }
 
     @Override
